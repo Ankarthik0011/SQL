543b4bb66257e7641d62a1a3ac37b1516e07f2b7
Karthik A N <ankarthik.531@gmail.com>
2025-10-31 18:44:38 +0530

    Add files via upload

---
+++ SQL/Sql 11111.sql
@@ -0,0 +1,17 @@
+insert into ise(name,position)
+values('Alice jhonson','software engineer'),
+('bob smith','project manager'),
+('carol white','data analyst');
+truncate table ise
+Select * from eee
+create database sap
+use sap
+create table demo(person_Id int,First_name varchar(200),last_name varchar(200),dept varchar(200),address varchar(200));
+Insert into demo values(001,"ba","bala","deals","cbe","balajipuram")
+Insert into demo values(002,"surya","siva","deals","biotech","barathinagar")
+Insert into demo values(003,"prabha","karan","deals","support","keerantham")
+Insert into demo values(004,"varsh","siva","deals","cbe","vidhyaranyapura")
+Insert into demo values(005,"guru","manoj","deals","devops","hesaragatta")
+Insert into demo values(006,"ghuu","manga","deals","devops","hesaragatta")
+alter table demo add email varchar(255)
+insert into
